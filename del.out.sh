074e35895cf3507e42bc4ec6c55645d727bef37d
James.Shi.CH <james.shi.ch@gmail.com>
2014-12-20 09:26:55 +0800

    auto upload by using shell script

--- del.out.sh
+++ del.out.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+
+# executable program
 find . -name *.out > to_del
+
+# input data, because some is very big
+find . -name in >> to_del
+
+# .o file
+find . -name *.o >> to_del
+
+# .swp file
+find . -name *.swp >> to_del
+
+cat to_del 
+echo '* using cat to_del | xargs rm to delete them.' 
